21e9eedb496dfce262b7d43d8c8a829cab676918
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2019-01-01 21:08:59 +0000

    Added popup definitions to Blooms

--- pedagogy/blooms/main.js
+++ pedagogy/blooms/main.js
@@ -1,3 +1,4 @@
+let EVENT;
 function setup() {
     noCanvas();
     for (stage in blooms) {
@@ -14,8 +15,8 @@ function setup() {
             }
             newCell = dom.createElementWithClass("TD", "cell");
             newCell.innerText =blooms[stage][i];
-            newCell.addEventListener("mouseover", function() {getDefinition(this.innerHTML)});
-            newCell.addEventListener("mouseout", function() {});
+            newCell.addEventListener("mouseover", function() {EVENT = event; getDefinition(this.innerHTML)});
+            newCell.addEventListener("mouseout", clearInfo);
             newRow.appendChild(newCell);
         }
         newTable.appendChild(newRow);
@@ -25,16 +26,42 @@ function setup() {
     }
 }
 
-wordnik_api_key = "4b99ccccd85102abe00090c7dd602c2e38fe2618b64fec3cd";
-wordnik_url_part1 = "https://api.wordnik.com/v4/word.json/";
-wordnik_url_part2 = "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=" + wordnik_api_key;
-word = "cabbage";
+let wordnik_api_key = "4b99ccccd85102abe00090c7dd602c2e38fe2618b64fec3cd";
+let wordnik_url_part1 = "https://api.wordnik.com/v4/word.json/";
+let wordnik_url_part2 = "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=" + wordnik_api_key;
+let word = "cabbage";
+
+
 
 function getDefinition(word) {
     request = wordnik_url_part1 + word + wordnik_url_part2;
-    loadJSON(request, gotData);
+    loadJSON(request, gotData, gotData);
 }
 
 function gotData(data) {
-    console.log(data["0"].text);
+    let d;
+    if (data["0"] != undefined) {
+        d = data["0"].text
+    } else {
+        d = "Oh dear, I don't know that one!";
+    }
+    displayInfoBox(d);
+}
+
+function displayInfoBox(d) {
+    let td = EVENT.srcElement;
+    let infoBox = document.getElementById("info-box");
+    dom.killChildren(infoBox);
+    infoBox.style.left = td.offsetParent.offsetLeft + td.offsetLeft + "px";
+    infoBox.style.top = td.offsetParent.offsetTop + td.offsetTop - (td.offsetHeight*2) +  "px";
+    let newP = document.createElement("P");
+    newP.innerHTML = d;
+    newP.classList.add("infoText"); 
+    infoBox.appendChild(newP);
+    dom.showElement(infoBox);
+}
+
+function clearInfo() {
+    let infoBox = document.getElementById("info-box");
+    dom.hideElement(infoBox);
 }
