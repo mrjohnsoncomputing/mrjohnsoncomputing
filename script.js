6f1a8d829c30f83e9f543dc19f1c7e218229e582
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2019-01-02 09:58:43 +0000

    Added Main Domain files

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let url = window.location.href;
+    if (url.indexOf("https") == -1) {
+        window.location.href = 'https://www.mrjohnsoncomputing.co.uk;'
+    }
+}
