f522fc16f0a1dd751330ae4fc42f29e633bdb6e3
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2019-01-04 16:09:49 +0000

    Added Basic Structure to ICT section

--- ICT/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    let url = window.location.href;
-    if (url.indexOf("https") == -1) {
-        window.location.href = 'https://www.mrjohnsoncomputing.co.uk;'
-    }
-}
