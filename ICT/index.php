2aaa0650e5dbe435c40928709fcc1e19eef35c47
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2019-01-07 21:17:24 +0000

    Typo

--- ICT/index.php
+++ ICT/index.php
@@ -1,6 +1,6 @@
 <?php include "navigation.php";?>
 <center>
     <h1 class="pageTitle">Information Communication Technology</h1>
-    <p>In our increasingly digital society, ICT is a fundamental skill to develop and becom proficient in.</p>
+    <p>In our increasingly digital society, ICT is a fundamental skill to develop and become proficient in.</p>
 </center>
 <?php include "footer.php";?>
